068cfa616c916f68fc906ed3c2314c07a477fe09
Venkatesh K <39611230+venkatk89@users.noreply.github.com>
2020-08-12 16:44:14 +0530

    Update data_prep.R

--- Dealing with Categorical Variables/data_prep.R
+++ Dealing with Categorical Variables/data_prep.R
@@ -1,17 +1,6 @@
-# load libraries
-
-library(ggplot2)
-library(dplyr)
-library(data.table)
-library(mltools)
-library(xgboost)
-
-
 # load data
-
 x_train <- read.csv("C:/Users/VENKATESH K/Desktop/flu_shot_learning/data/training_set_features.csv")
 y_train <- read.csv("C:/Users/VENKATESH K/Desktop/flu_shot_learning/data/training_set_labels.csv")
-
 
 x_test <- read.csv("C:/Users/VENKATESH K/Desktop/flu_shot_learning/data/test_set_features.csv")
 submission <- read.csv("C:/Users/VENKATESH K/Desktop/flu_shot_learning/data/submission_format.csv")
@@ -20,9 +9,11 @@ submission <- read.csv("C:/Users/VENKATESH K/Desktop/flu_shot_learning/data/subm
 # merge train dataset
 train <- merge(x_train, y_train, by = "respondent_id")
 
+
 # convert all columns (including dependent variables) to categorical variables
 train <- sapply(train, as.factor)
 train <- as.data.frame(train)
+
 
 # change dependent variables (also id) to numeric variable
 train$respondent_id <- as.numeric(train$respondent_id)
@@ -32,7 +23,6 @@ train$seasonal_vaccine <- as.numeric(train$seasonal_vaccine)
 
 # all changes done to train dataset is also done to test dataset
 test_id <- x_test$respondent_id
-
 x_test <- sapply(x_test, as.factor)
 x_test <- as.data.frame(x_test)
 x_test$respondent_id <- test_id
@@ -42,18 +32,13 @@ x_test$respondent_id <- test_id
 #write.csv(train, "train.csv", row.names = FALSE)
 
 
-
-
 # features segregation
-
 h1n1_features <- c("h1n1_concern", "h1n1_knowledge" ,
                    "doctor_recc_h1n1", "opinion_h1n1_vacc_effective",
                    "opinion_h1n1_risk", "opinion_h1n1_sick_from_vacc")
 
 seasflu_features <- c("doctor_recc_seasonal", "opinion_seas_vacc_effective",
                       "opinion_seas_risk", "opinion_seas_sick_from_vacc")
-
-
 
 behavioral_features <- c("behavioral_antiviral_meds", "behavioral_avoidance",
                      "behavioral_face_mask", "behavioral_wash_hands" ,
@@ -63,14 +48,11 @@ behavioral_features <- c("behavioral_antiviral_meds", "behavioral_avoidance",
 health_features <- c("chronic_med_condition", "child_under_6_months", 
                      "health_worker", "health_insurance")
 
-
-
 demo_features <- c("age_group"  ,"education" ,"race" ,"sex" ,"income_poverty" ,
                    "marital_status" ,"rent_or_own" ,"employment_status" ,
                    "hhs_geo_region" ,"census_msa" ,"household_adults"  ,
                    "household_children" , "employment_industry" ,
                    "employment_occupation")
-
 
 
 # variables to use to predict h1n1 vaccination
@@ -80,6 +62,7 @@ h1n1_vars <- c(h1n1_features, behavioral_features, health_features, demo_feature
 seasflu_vars <- c(seasflu_features, behavioral_features, health_features, demo_features)
 
 
+# make new dataframes (easier for modelling later on)
 h1n1_train <- train[,h1n1_vars]
 h1n1_y <- as.numeric(train$h1n1_vaccine) - 1
 h1n1_test <- x_test[,h1n1_vars]
@@ -87,10 +70,6 @@ h1n1_test <- x_test[,h1n1_vars]
 sesflu_train <- train[,seasflu_vars]
 seasflu_y <- as.numeric(train$seasonal_vaccine) - 1
 seasflu_test <- x_test[,seasflu_vars]
-
-
-factor_features <- c(behavioral_features, demo_features, h1n1_features, health_features, seasflu_features)
-
 
 
 # uncomment following lines to write data to local memory
